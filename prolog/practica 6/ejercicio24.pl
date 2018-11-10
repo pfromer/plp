b364c0579f44ceb4e5c0fe1b6a403c8b7e040a7b
pfromer <pablofromer@gmail.com>
2018-11-10 20:27:51 -0300

    prolog

--- prolog/practica 6/ejercicio24.pl
+++ prolog/practica 6/ejercicio24.pl
@@ -1,4 +1,9 @@
+/*
 arbol(nil).
-arbol(bin(nil,V,T)) :- arbol(bin(nil,V,T)), arbol(T).
-arbol(bin(T,V,nil)) :- T\=nil, arbol(T).
+arbol(bin(nil,V2,bin(T,V1,T2))) :- arbol(T), arbol(T2).
+arbol(bin(T,V1,T2),V2,nil) :- arbol(T), arbol(T2).
 arbol(bin(T,V,T2)) :- arbol(T), arbol(T2).
+*/
+
+arbol(nil).
+arbol(bin(T,V2,nil)) :- arbol(T).
